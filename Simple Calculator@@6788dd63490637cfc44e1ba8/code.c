a2b314bc20b2fad92227a04c43c4c2355c0ebacc
Harshil1812 <mahajanharshil2005@gmail.com>
2025-02-13 17:28:34 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
-int main(){
-    char z;
-    scanf("%c",&z)
-    int x , y;
-    scanf("%d %d",&x, &y);
-    switch(z) {
+
+int main() {
+    char operator;
+    float num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (operator) {
         case '+':
-        result = x + y;
-        printf("%d", x , y ,result);
-        break;
-         case '-':
-        result = x - y;
-        printf("%d", x , y ,result);
-        break;
+            result = num1 + num2;
+            printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+            break;
         case '*':
-        result = x * y;
-        printf("%d", x , y ,result);
-        break;
+            result = num1 * num2;
+            printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+            break;
         case '/':
-        result = x / y;
-        printf("%d", x , y ,result);
-        break;
-        else {
-            printf("error");
-        }
-
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2f / %.2f = %.2f\n", num1, num2, result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator\n");
+            break;
     }
-    return 0;
 
+    return 0;
 }
